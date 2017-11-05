5fe074309e91aed710f6782d32a2ecd04c2b6be3
Twitch <ianletwitch@gmail.com>
2017-11-05 17:08:36 +1100

    Update main.go

--- main.go
+++ main.go
@@ -18,6 +18,20 @@ var regularHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Reques
 })
 
 func main() {
+    authErauthErr := jwt.New(&restrictedRoute, jwt.Options{
+    SigningMethodString:   "RS256",
+    PrivateKeyLocation:    "keys/app.rsa",     // `$ openssl genrsa -out app.rsa 2048`
+    PublicKeyLocation:     "keys/app.rsa.pub", // `$ openssl rsa -in app.rsa -pubout > app.rsa.pub`
+    RefreshTokenValidTime: 72 * time.Hour,
+    AuthTokenValidTime:    15 * time.Minute,
+    Debug:                 false,
+    IsDevEnv:              true,
+    })
+    if authErr != nil {
+        log.Println("Error initializing the JWT's!")
+        log.Fatal(authErr)
+    }
+    
     http.HandleFunc("/", regularHandler)
     http.Handle("/restricted", restrictedHandler)
 
