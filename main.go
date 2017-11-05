bae140378cefaa5372cc301b66d1f27e90390818
Twitch <ianletwitch@gmail.com>
2017-11-05 14:13:52 +1100

    Update main.go

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
     "os"
 )
 
+var restrictedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Welcome to the secret area!"))
+})
+
 var regularHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte("Hello, World!"))
 })
 
 func main() {
     http.HandleFunc("/", regularHandler)
+    http.Handle("/restricted", restrictedHandler)
+
+    
     http.ListenAndServe(":"+os.Getenv("PORT") ,nil)
 }
