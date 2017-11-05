497d74d3d0c60142d169f4b278aaf4cd46cc00c9
Twitch <ianletwitch@gmail.com>
2017-11-05 12:49:32 +1100

    Testing jwt-auth with the Quickstart

--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
-    "fmt"
     "net/http"
-    "os"
+    "log"
+    "time"
+
+    "github.com/adam-hanna/jwt-auth/jwt"
 )
 
-func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
-}
+var restrictedRoute jwt.Auth
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+var restrictedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Welcome to the secret area!"))
+})
+
+var regularHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Hello, World!"))
+})
+
+func main() {
+  authErr := jwt.New(&restrictedRoute, jwt.Options{
+    SigningMethodString:   "RS256",
+    PrivateKeyLocation:    "keys/app.rsa",     // `$ openssl genrsa -out app.rsa 2048`
+    PublicKeyLocation:     "keys/app.rsa.pub", // `$ openssl rsa -in app.rsa -pubout > app.rsa.pub`
+    RefreshTokenValidTime: 72 * time.Hour,
+    AuthTokenValidTime:    15 * time.Minute,
+    Debug:                 false,
+    IsDevEnv:              true,
+  })
+  if authErr != nil {
+    log.Println("Error initializing the JWT's!")
+    log.Fatal(authErr)
+  }
+
+  http.HandleFunc("/", regularHandler)
+  // this will never be available because we never issue tokens
+  // see login_logout example for how to provide tokens
+  http.Handle("/restricted", restrictedRoute.Handler(restrictedHandler))
+
+  log.Println("Listening on localhost:3000")
+  http.ListenAndServe("127.0.0.1:3000", nil)
 }
