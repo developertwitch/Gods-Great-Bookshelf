5055955e1d8e57bc305f069f41fab664edb57a70
Twitch <ianletwitch@gmail.com>
2017-11-05 17:16:53 +1100

    Update main.go

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var regularHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Reques
 
 func main() {
     authErr := jwt.New(&restrictedRoute, jwt.Options {
-        SigningMethodStrin: "RS256",
+        SigningMethodString: "RS256",
         PrivateKeyLocation: "keys/app.rsa",     // `$ openssl genrsa -out app.rsa 2048`
         PublicKeyLocation: "keys/app.rsa.pub", // `$ openssl rsa -in app.rsa -pubout > app.rsa.pub`
         RefreshTokenValidTime: 72 * time.Hour,
