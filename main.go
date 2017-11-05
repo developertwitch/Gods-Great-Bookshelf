8cfd11bf2ccd74708b1b1fdb0066e75bef713af8
Twitch <ianletwitch@gmail.com>
2017-11-05 13:45:47 +1100

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-    "net/http"
     "os"
     "fmt"
 )
 
-func regularHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, world!")
-}
-
 func main() {
-  http.HandleFunc("/", regularHandler)
-    
-  http.ListenAndServe(":"+os.Getenv("POST"), nil)
+    fmt.Fprintf(os.Getenv("PORT")
 }
