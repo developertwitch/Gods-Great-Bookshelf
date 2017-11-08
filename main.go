071e54445a4ceb0d1fa14bf6e579d00507762be2
Twitch <ianletwitch@gmail.com>
2017-11-08 22:41:56 +1100

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
     "time"
     
     "github.com/adam-hanna/jwt-auth/jwt"
-    "github.com/developertwitch/gods-great-bookshelf/templates"
+    "github.com/developertwitch/gods-great-bookshelf/tree/master/templates"
 )
 
 var restrictedRoute jwt.Auth
